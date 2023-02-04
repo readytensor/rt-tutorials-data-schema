33fbf74067ed9275ae33bbf07a993389fe9feb6d
Abu Desai <abhyuday.desai@gmail.com>
2023-02-03 23:06:55 -0800

    replace employee_attrition data with titanic

--- app/run_script.py
+++ app/run_script.py
@@ -1,16 +1,15 @@
 from schema_provider import BinaryClassificationSchema
 
-
-def read_schema(): 
+def read_schema():
     """Reads the binary classification schema."""
 
     # path to the schema file
-    schema_fpath = "./inputs/employee_attrition_schema.json"
+    schema_fpath = "./inputs/titanic_schema.json"
 
-    # instantiate schema object which loads the json file schema
+    # instantiate schema provider which loads the json file schema
     bc_schema = BinaryClassificationSchema(schema_fpath)
-    
-    # check if schema_reader parsed the schema correctly
+
+    # check if schema provider parsed the schema correctly
     print(f"id_field is: `{bc_schema.id_field}`")
     print(f"target_field is: `{bc_schema.target_field}`")
     print(f"target_class is: `{bc_schema.target_class}`")
@@ -19,5 +18,5 @@ def read_schema():
     print(f"All fields are: {bc_schema.all_fields}")
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     read_schema()
